6e95e730f682b520c3b01288ea7498a854b57eb1
Rodrigo Cerqueira <rbcerqueira@gmail.com>
2020-06-04 23:56:23 -0300

    Adicionano www diretório Adicionando json territórios de identidade Atualizando server.R

---
+++ server.R
@@ -0,0 +1,266 @@
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(tidyr)
+library(plotly)
+library(ggplot2)
+library(rgdal)
+library(sf)
+library(ggthemes)
+library(RColorBrewer)
+library(shinydashboardPlus)
+library(sp)
+#library(d3tree)
+library(treemap)
+library(viridis)
+library(leaflet)
+
+#Fonte do grafico PLOTLY - Cleiton
+fonte_plotly <- list(family = "sans serif",size = 12, color = 'black')
+
+#DataSets necessários para geração dos gráficos PIB ANUAL - RODRIGO
+PIBanual <- read.csv2("pib_anual.csv", dec=",", h=T)
+PIBanualBA <- subset(PIBanual, CodUF ==29)
+PIBanualBA$cor <- ifelse(PIBanualBA$tx <0, "positivo", "negativo")
+PIBsetores <- read.csv2("pib_setores.csv", dec=",", h=T)
+PIBsetores$cor <- ifelse(PIBsetores$tx <0, "positivo", "negativo")
+PIBbrneba <- subset(PIBanual, UF=="Bahia" | UF=="Brasil" | UF=="Nordeste")
+PIBbrneba <- PIBbrneba %>% filter(Ano!=2018)
+f <- list(size=11)
+pibmunicipios <-read.csv2("pib_municipios.csv", dec=",")
+#dadosmapa <- read.csv2("dados_e_mapas.csv")
+
+
+#invertendo banco para gráfico de pizza
+pibmunicipios_pizza <- pibmunicipios %>% gather(SETOR, PARTICIP,AGRICULTURA:SERVIÇOS)
+
+#importando arquivo json - BRUNO
+municipio_bahia <- read_sf("plgn_municipios_bahia.json")#, options = "ENCODING=UTF-8")
+
+#convertendo variavel de caracter em numerica - ABA 1 - BRUNO
+municipio_bahia$cd_geocmu <- as.numeric(municipio_bahia$cd_geocmu)
+
+#calcular pontos nos quais plotar etiquetas - ABA 1 - BRUNO
+centroids <- municipio_bahia %>% st_centroid() %>% bind_cols(as_data_frame(st_coordinates(.)))
+
+#criando variavel categorizando o pibpercentual - ABA 1 - BRUNO
+pibmunicipios$categoria_percentual <- cut(pibmunicipios$pib_percentual, 
+                                          breaks=c(0,0.05,0.10,0.5,1,5,100), 
+                                          labels=c("0 - 0,05%", "0,05% - 0,1%", "0,1% - 0,5%", 
+                                                   "0,5% - 1%","1% - 5%", ">5%"))
+
+pibmunicipios$categoria_percentual_agro <- cut(pibmunicipios$PERCENTUAL_AGRO_BA, 
+                                               breaks=c(0,0.0005,0.0010,0.005,0.01,0.05,1), 
+                                               labels=c("0 - 0,05%", "0,05% - 0,1%", "0,1% - 0,5%", 
+                                                        "0,5% - 1%","1% - 5%", ">5%"))
+
+pibmunicipios$categoria_percentual_ind <- cut(pibmunicipios$PERCENTUAL_IND_BA, 
+                                              breaks=c(0,0.0005,0.0010,0.005,0.01,0.05,1), 
+                                              labels=c("0 - 0,05%", "0,05% - 0,1%", "0,1% - 0,5%", 
+                                                       "0,5% - 1%","1% - 5%", ">5%"))
+
+pibmunicipios$categoria_percentual_serv <- cut(pibmunicipios$PERCENTUAL_SERV_BA, 
+                                               breaks=c(0,0.0005,0.0010,0.005,0.01,0.05,1), 
+                                               labels=c("0 - 0,05%", "0,05% - 0,1%", "0,1% - 0,5%", 
+                                                        "0,5% - 1%","1% - 5%", ">5%"))
+
+#paleta
+wardpal <- colorFactor (palette = ("Purples"), pibmunicipios$categoria_percentual)
+warpal2 <- colorFactor(palette = ("Greens"), pibmunicipios$categoria_percentual_agro)
+wardpal3 <- colorFactor(palette = ("Oranges"), pibmunicipios$categoria_percentual_ind)
+wardpal4 <- colorFactor(palette = ("Red"), pibmunicipios$catetoria_percentual_serv)
+
+#banco unindo mapa com os dados - ABA 1 # - BRUNO
+dados_e_mapa <- pibmunicipios %>% left_join(municipio_bahia, ., by ='cd_geocmu')
+
+dados_e_mapa$textomapa <- paste0(dados_e_mapa$MUNICIPIO, " = ", round(dados_e_mapa$pib_percentual,2), "%")
+
+
+#dadosmapa <- dados_e_mapa %>% filter (ANO==2016)
+
+#PIB anual com separador de milhar - RODRIGO
+PIBanual$PIB <- format(round(as.numeric(PIBanual$PIB), 1), nsmall=0,  big.mark=".", decimal.mark=",")
+
+#server
+function(input, output, session) {
+  
+  ######################################################################################
+  # PAGINA INFORMACOES GERAIS
+  ######################################################################################
+  
+  #Caixa com Populacao total da Bahia
+  output$PIBtotalBA <- renderValueBox({
+    valueBox(
+      paste("R$",subset(x=PIBanual,subset=(CodUF==29 & Ano==input$selectano),select = c(PIB)),"Milhões"),"Produto Interno Bruto do Estado", 
+      icon = icon("dollar-sign"),
+      color = "navy"
+    )
+  })
+  #Caixa com a posicao da Bahia no ranking do Brasil
+  output$particip_Bahia_Brasil <- renderValueBox({
+    valueBox(
+      if (is.na(subset(x=PIBanual,subset=(CodUF==29 & Ano==input$selectano),select = c(particip_br)))) {
+        paste0("-")
+      } else {
+        paste0(format(subset(x=PIBanual,subset=(CodUF==29 & Ano==input$selectano),select = c(particip_br)),nsmall=0,  big.mark=".", decimal.mark=","),"% do Brasil")
+      },
+      "participação no Brasil", icon = icon("globe-americas"),
+      color = "purple"
+    )
+  })
+  
+  #Caixa com a posicao da Bahia no ranking do Nordeste
+  output$particip_Bahia_NE <- renderValueBox({
+    valueBox(
+      if (is.na(subset(x=PIBanual,subset=(CodUF==29 & Ano==input$selectano), select=c(particip_ne)))) {
+        paste0("-") 
+      } else {
+        paste0(format(subset(x=PIBanual,subset=(CodUF==29 & Ano==input$selectano),select =c(particip_ne)),nsmall=0,  big.mark=".", decimal.mark=","),"% do NE")
+      },
+      #paste0(format(subset(x=PIBanual,subset=(CodUF==29 & Ano==input$selectano),select =c(particip_ne)),nsmall=0,  big.mark=".", decimal.mark=","),"% do NE"),
+      "participação no Nordeste", icon = icon("umbrella-beach"),
+      color = "maroon"
+    )
+  })
+  
+  #Caixa com a taxa de fecundidade em 2019
+  output$tx_cresc <- renderValueBox({
+    valueBox(
+      paste0(format(subset(x=PIBanual,subset=(CodUF==29 & Ano==input$selectano),select = c(tx)), nsmall=0,  big.mark=".", decimal.mark=","),"%"),"em relação ao ano anterior", icon = icon("percent"),
+      color = "orange"
+    )
+  })
+  
+  #Gráficos de Barras - Crescimento do VA dos Setores
+  output$tx_setores <- renderPlot({
+    ggplot(subset(PIBsetores, subset = (Ano==input$selectano)), aes(x=setor, y=tx))+
+      geom_bar(stat= "identity", aes(fill =cor), width = 0.9, show.legend = F)+
+      geom_text(aes(x=setor, y=tx, label = tx))+
+      theme_bw()+
+      labs(title= "Taxa de Crescimento do Valor Adicionado dos setores no ano", x = "", y= "Taxa de Crescimento (%)",
+           caption = "Fonte: SEI-IBGE")+
+      theme(axis.text.x = element_text(angle = 65, hjust = 1, size=11), axis.text.y = element_text())+
+      scale_fill_manual(values = c("#0fabbc", "#fa163f"))
+    
+  })
+  
+  #Grafico de Radar (teia de aranha) estrutura dos setores
+  output$radar_pib <- renderPlotly({ 
+    PIBsetores %>% subset(Ano==input$selectano) %>%
+      plot_ly() %>%
+      add_trace(type = "scatterpolar",
+                mode = "lines",
+                r= ~estrutura,
+                theta= ~setor,
+                fill="toself",
+                fillcolor="#83af70",
+                opacity=0.9,
+                line=list(color="#488f31", width=4),
+                hoverinfo="estrutura") %>%
+      layout(polar=list(radialaxis=list(visible=T, range = c(0,20))), showlegend=F, 
+             title=list(text="Participação percentual (%) dos Setores no Valor Adicionado", x=0,
+                        font=list(size=12, face="bold")))
+  })
+  
+  #Grafico de barras - crescimento PIB
+  output$tx_bahia <- renderPlot({
+    ggplot(PIBanualBA, aes(x=factor(Ano), y=tx)) +
+      theme_bw()+
+      geom_bar(stat="identity", aes(fill = cor), show.legend = F)+
+      geom_text(aes(x=factor(Ano), y=tx, label =tx))+
+      labs(x = "Ano", y="Taxa de Crescimento (%)")+
+      labs(title = "Taxa de Crescimento do PIB anual (2002 - 2017)", caption = "Fonte: SEI-IBGE")+
+      theme(axis.text.x = element_text(vjust = 0.6), axis.text.y = element_text())+
+      scale_fill_manual(values = c("#0fabbc", "#fa163f"))
+    
+  })
+  
+  #Série encadeada do volume do PIB - BA, BR, NE
+  output$serie_ba_br_ne <- renderPlot({ 
+    ggplot(PIBbrneba, aes(x=Ano, y=indice))+
+      geom_point(aes(color=UF), size=rel(3.5), shape=16)+
+      geom_line(aes(color=UF), size=rel(1.4))+ 
+      scale_x_continuous(breaks =seq( from=2002, to=2017, by=1))+
+      labs(x="Ano", y="",title =
+             "Série encadeada do volume do Produto interno bruto (Base: 2002=100)", color="",caption="Fonte: SEI-IBGE")+
+      theme_bw()+
+      scale_colour_manual(values = c("#ffa600","#58508d","#bc5090"))
+    
+    
+  })
+  
+  
+  #######################################################################################
+  # PAGINA TRIMESTRAL
+  #######################################################################################
+  
+
+  
+  #######################################################################################
+  # PAGINA MUNICIPAL
+  #######################################################################################  
+  
+  #Caixa com PIB do Município
+  output$PIBtotalMunicipio <- renderValueBox({
+    valueBox(
+      paste("R$",format(subset(x=pibmunicipios, subset=(MUNICIPIO==input$selectmunicipio & ANO==input$sliderano2),select = c(PIB)),nsmall=0,  big.mark=".", decimal.mark=","),"Milhões"),"Produto Interno Bruto do Município", 
+      icon = icon("dollar-sign"),
+      color = "navy"
+    )
+  })
+  
+  #Caixa com PIB per capita
+  output$PIBpercapita <- renderValueBox({
+    valueBox(
+      paste0("R$",format(subset(x=pibmunicipios, subset=(MUNICIPIO==input$selectmunicipio & ANO==input$sliderano2), select = c(PIB_PERCAPITA)),nsmall=0,  big.mark=".", decimal.mark=",")),"Produto Interno Bruto per capita", 
+      icon = icon("users"),
+      color = "purple"
+    )
+  })
+  
+  #Caixa com Participação dos Municípios Bahia
+  output$PIBparticip <- renderValueBox({
+    valueBox(
+      paste0(format(subset(x=pibmunicipios, subset=(MUNICIPIO==input$selectmunicipio & ANO==input$sliderano2),select = c(pib_percentual)),nsmall=0,  big.mark=".", decimal.mark=","),"%"),"Participação do Município no PIB BA", 
+      icon = icon("percent"),
+      color = "maroon"
+    )
+  })
+  
+  #Caixa com IDEM dos Municípios Bahia
+  output$IDEM <- renderValueBox({
+    valueBox(
+      paste0(format(subset(x=pibmunicipios, subset=(MUNICIPIO==input$selectmunicipio & ANO==input$sliderano2),select = c(IDEM)),nsmall=0,  big.mark=".", decimal.mark=",")),"IDEM do Município no PIB BA", 
+      icon = icon("chart-line"),
+      color = "orange"
+    )
+  })
+  
+  #MAPA PIB
+  marcadormunicipio <- 
+    
+    output$mapa_pib <- renderLeaflet({
+      leaflet(subset(x=dados_e_mapa, subset=(ANO==input$sliderano2))) %>%
+        addTiles() %>%
+        addMarkers(subset(x=dados_e_mapa, subset = (MUNICIPIO==input$selectmunicipio))$geometry) %>%
+        setView(lat = -13.591215, lng = -37.979077, zoom = 5.5) %>% 
+        addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 1,
+                    fillColor = ~wardpal(categoria_percentual),
+                    label = ~paste0(MUNICIPIO, ": ", format(pib_percentual, big.mark = ".",decimal.mark=","), "%")) %>%
+        addLegend("bottomright",pal = wardpal, values = ~categoria_percentual, opacity = 1.0, title = "Tamanho da População")
+      
+    })
+  
+  #PIZZA Municípios
+  output$municip_pizza <- renderPlot({
+    ggplot(subset(pibmunicipios_pizza, subset=(ANO==input$sliderano2 & MUNICIPIO==input$selectmunicipio)), aes(x="", y=PARTICIP, fill= SETOR)) +
+      geom_bar(stat="identity", width=1, color="white") +
+      coord_polar("y", start=0) + 
+      theme_bw() + 
+      theme_void()+
+      ggtitle("Participação dos setores da economia") +
+      geom_text(aes(y = PARTICIP, label = SETOR), color = "white", size=6)
+  })
+  
+  
+}
---
+++ ui.R
@@ -0,0 +1,194 @@
+#install.packages(c("shiny","shinydashboard","shinyjs","plotly"))
+
+#Carregando Pacote para fazer aplicativos shiny
+library(shiny)
+library(shinydashboard)
+library(plotly)
+library(lubridate)
+library(shinydashboardPlus)
+library(rgdal)
+library(sf)
+#library(d3tree)
+library(leaflet)
+devtools::install_github("ricardo-bion/ggradar", 
+                         dependencies = TRUE)
+
+
+#Preparando algumas listas necessarias
+seqano <- as.list(2002:2018) ; names(seqano) <- 2002:2018
+Anoatual <- 2017
+pib_municipios <- read.csv2("pib_municipios.csv", dec = ",")
+municipiolist <- pib_municipios$MUNICIPIO
+setorlist <- as.list(c("categoria_percentual","categoria_percentual_agro", "categoria_percentual_ind", "categoria_percentual_serv"))
+
+#ui
+dashboardPagePlus(skin = "blue", title="SEI - PIB",
+                  header = dashboardHeaderPlus(
+                    title = tagList(
+                      span(class = "logo-lg", img(src = "LogoGovBaTransp.png", width = "147.46px", height = "40px")), 
+                      img(src = "SEI_transparente.png",
+                          width = "30px", height = "30px")
+                    )
+                  ),
+                  sidebar = dashboardSidebar(collapsed = TRUE,
+                                             
+                                             sidebarMenu(
+                                               menuItem("Anual", tabName = "aba1", icon = icon("chart-bar")),
+                                              # menuItem("Trimestral", tabName = "aba2", icon = icon("chart-line")),
+                                               menuItem("Municipal", tabName = "aba3", icon = icon("map-marked-alt")),           
+                                               menuItem("Temático", tabName = "aba4", icon = icon("chart-pie"),
+                                                        menuSubItem("Agronegócio", tabName = "aba6", icon = icon("leaf")),
+                                                        menuSubItem("Agricultura Familiar", tabName = "aba7", icon = icon("seedling")),
+                                                        menuSubItem("Turismo", tabName = "aba8", icon = icon("luggage-cart")),
+                                                        menuSubItem("Cultura", tabName = "aba9", icon = icon("theater-masks")),
+                                                        menuSubItem("Saúde", tabName = "aba10", icon = icon("ambulance"))),
+                                               menuItem("Desenvolvedores", tabName = "aba5", icon = icon("code"))
+                                             )
+                                             
+                  ),   
+                  dashboardBody(
+                    tags$head(
+                      tags$link(rel="sortcut icon", href="LogoGovBa.png", type="image/png"),
+                      tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+                      #includeHTML("google-analytics.html")
+                    ),
+                    tabItems(
+                      
+                      #################################################################################
+                      #Pagina Anual
+                      #################################################################################
+                      
+                      tabItem(tabName = "aba1", h3("PIB Anual"),
+                              selectInput(inputId="selectano", label=h4("Selecione o Ano"), choices = seqano, selected = 2017),
+                              fluidRow(valueBoxOutput("PIBtotalBA", width=3), 
+                                       valueBoxOutput("particip_Bahia_Brasil", width=3),
+                                       valueBoxOutput("particip_Bahia_NE", width = 3),
+                                       valueBoxOutput("tx_cresc", width = 3)), br(),
+                              fluidRow(
+                                column(width=6,
+                                       box(
+                                         width = NULL,
+                                         title = paste("Taxa de Crescimento Anual dos Setores"),
+                                         status = "primary",
+                                         plotOutput("tx_setores"),
+                                         footer="Fonte: SEI-IBGE"
+                                       )
+                                ),
+                                column(width=6,
+                                       box(
+                                         width=NULL,
+                                         status="primary",
+                                         title="Radar dos Setores, PIB Anual",
+                                         plotlyOutput("radar_pib"),
+                                         footer = "Fonte: SEI"
+                                       )
+                                )), br(),
+                              fluidRow(
+                                column(width=6,
+                                       box(
+                                         width=NULL,
+                                         title="Taxa de Crescimento do PIB Anual (2002-2017), Bahia",
+                                         plotOutput("tx_bahia"),
+                                         status="primary",
+                                         footer="Fonte: SEI-IBGE"
+                                       )
+                                ),
+                                column(width=6,
+                                       box(width=NULL,
+                                           plotOutput("serie_ba_br_ne"),
+                                           title="Série encadeada do volume do PIB (base 2002=100)",
+                                           status="primary",
+                                           footer="Fonte: SEI-IBGE"
+                                       )
+                                       
+                                ))
+                      ),
+                      
+                      #################################################################################
+                      #Pagina Trimestral
+                      #################################################################################
+                      tabItem(tabName = "aba2", 
+                              sliderInput(inputId="sliderano", label=h4("Selecione o ano"), min=2010, max=2060, value=Anoatual, step = NULL, round = TRUE,
+                                          ticks = TRUE, animate = TRUE,
+                                          width = 1080, sep = ".", pre = NULL, post = NULL, timeFormat = NULL,
+                                          timezone = NULL, dragRange = TRUE),
+                              fluidRow(valueBoxOutput("poptotalproj", width=3), 
+                                       valueBoxOutput("tftproj", width=3),
+                                       valueBoxOutput("EVN", width=3), 
+                                       valueBoxOutput("MortInf", width=3)
+                              ),
+                              fluidRow(
+                                column(width=6,
+                                       fluidRow(plotOutput("piramide"))
+                                ),
+                                column(width=6,plotOutput("EVNporSexo"))
+                              ), br(),
+                              fluidRow(plotOutput("GrafPopTotal")),br(),
+                              fluidRow(plotOutput("TaxasBrutas"))
+                      ),
+                      
+                      #################################################################################
+                      #Pagina Municipal
+                      #################################################################################
+                      tabItem(tabName = "aba3", 
+                              sliderInput(inputId="sliderano2", label=h4("Selecione o ano"), min=2002, max=2017, 
+                                          value=2002, step = NULL, round = TRUE, ticks = TRUE, 
+                                          animate = animationOptions(interval=2500), width = 1080, sep = ".", pre = NULL, 
+                                          post = NULL, timeFormat = NULL,timezone = NULL, dragRange = TRUE),
+                              fluidRow(column(width = 2,selectInput(inputId="selectmunicipio", label=h4("Selecione o Município"), choices = municipiolist, selected = 2000)),
+                                       column(width = 4,selectInput(inputId="selectsetor", label = h4("Selecione o Setor"), choices = setorlist))),
+                              fluidRow(valueBoxOutput("PIBtotalMunicipio", width=3),
+                                       valueBoxOutput("PIBpercapita", width=3),
+                                       valueBoxOutput("PIBparticip", width=3),
+                                       valueBoxOutput("IDEM", width = 3)),
+                              fluidRow(
+                                column(width=6, leafletOutput("mapa_pib"), br()),
+                                column(width=6, plotOutput("municip_pizza"))
+                              )
+                      ),
+                      
+                      
+                      #################################################################################
+                      #Pagina resultados do registro civil
+                      #################################################################################
+                      
+                      tabItem(tabName = "aba6", "Aqui estara os resultados do RC"),
+                      
+                      #################################################################################
+                      #Desenvolvedores
+                      #################################################################################
+                      
+                      tabItem(tabName = "aba5", h3("Desenvolvedores"), br(),
+                              fluidRow(column(width=6,
+                                              widgetUserBox(
+                                                title = "Rodrigo Barbosa de Cerqueira",
+                                                subtitle = "Especialista em Produção de Informações Econômicas",
+                                                type = NULL,
+                                                width = 12,
+                                                src = "jonatas.jpg",
+                                                color = "aqua-active",
+                                                closable = FALSE,
+                                                "",
+                                                footer = "Mestre em Economia pela UFBA. Técnico da Coordenação de Contas
+                                                Regionais e Finanças Públicas (COREF) da SEI."
+                                              )),
+                                       column(width=6,
+                                              widgetUserBox(
+                                                title = "Bruno Brasil",
+                                                subtitle = "Estagiário",
+                                                type = NULL,
+                                                width = 12,
+                                                src = "cleiton.jpg",
+                                                color = "aqua-active",
+                                                closable = FALSE,
+                                                " ",
+                                                footer = "Estudante de Economia da UNIFACS. Estagiário da Coordenação de Contas
+                                                Regionais e Finanças Públicas (COREF) da SEI."
+                                              )))
+                      )
+                      
+                      
+                    )
+                    
+                  )
+)
